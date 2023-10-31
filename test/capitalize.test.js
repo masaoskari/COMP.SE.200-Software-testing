6d32bda92b800bcf0c8add4b43283570e8d2f8cf
masaoskari <masaoskari.linna@gmail.com>
2023-10-31 10:59:49 +0200

    add other files

---
+++ test/capitalize.test.js
@@ -0,0 +1,19 @@
+import capitalize from './capitalize';
+
+describe('capitalize', () => {
+  it('should capitalize the first character of a string and convert the rest to lowercase', () => {
+    expect(capitalize('FRED')).toBe('Fred');
+    expect(capitalize('hello')).toBe('Hello');
+    expect(capitalize('wORLD')).toBe('World');
+  });
+
+  it('should handle empty strings', () => {
+    expect(capitalize('')).toBe('');
+  });
+
+  it('should handle non-string inputs', () => {
+    expect(capitalize(null)).toBe('Null');
+    expect(capitalize(undefined)).toBe('Undefined');
+    expect(capitalize(123)).toBe('123');
+  });
+});
