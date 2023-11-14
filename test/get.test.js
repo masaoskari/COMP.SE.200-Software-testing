5a8e663fbc4cee60fddd548c21ccef746bbc94cb
masaoskari <masaoskari.linna@gmail.com>
2023-11-14 16:24:42 +0200

    Fix get.test.js

--- test/get.test.js
+++ test/get.test.js
@@ -1,7 +1,6 @@
 import get from '../src/get.js';
-const { toBe, toEqual, not } = require('jest-extended');
-const { matchers } = require('jest-chain');
-expect.extend({ ...matchers });
+import 'jest-extended';
+import 'jest-chain';
 
 /**
  * Equivalence classes:
@@ -14,15 +13,15 @@ expect.extend({ ...matchers });
  * 7. Default value is not provided
  * 8. Object is null or undefined
  * 9. Path is null or undefined
- * 9. Value is object
- * 10 Value is array
+ * 9. Result is object
+ * 10 Result is array
  * 
  */
 
 describe('get', () => {
   test('should get property value by string path', () => {
     const object = { 'a': [{ 'b': { 'c': 3 } }] };
-    expect(get(object, 'a[0].b.c')).toBe(3);
+    expect(get(object, 'a[0].b.c')).toBe(3).toBeNumber();
   });
 
   test('should get property value from more complex object by string path', () => {
@@ -32,7 +31,7 @@ describe('get', () => {
 
   test('should get property value by array path', () => {
     const object = { 'a': [{ 'b': { 'c': 3 } }] };
-    expect(get(object, ['a', '0', 'b', 'c'])).toBe(3);
+    expect(get(object, ['a', '0', 'b', 'c'])).toBe(3).toBeNumber();
   });
 
   test('should get property value from more complex object by array path', () => {
@@ -45,33 +44,33 @@ describe('get', () => {
     expect(get(object, 'a.b.c', 'default')).toBe('default');
   });
 
-  test('should return undefined for null or undefined object', () => {
+  test('should return default for null or undefined object if default parameter is given', () => {
     expect(get(null, 'a.b.c', 'default')).toBe('default');
     expect(get(undefined, 'a.b.c', 'default')).toBe('default');
+    expect(get(null, 'a.b.c')).toBeUndefined();
   });
 
   test('should handle non-object values', () => {
-    expect(get([3], 0, 'default')).toBe(3); //is that bug?
     expect(get(3, 'a.b.c', 'default')).toBe('default');
-    expect(get('abc', '1', 'default')).toBe('b');
+    expect(get('abc', 1, 'default')).toBe('b');
+    expect(get(3, 0, 'default')).toBe('default');
+    expect(get(true, 0, 'default')).toBe('default');
   });
-  //expect(get(object, null, 'default')).toBe('default');
-  //expect(get(object, undefined, 'default')).toBe('default');
+
   test('should handle non-string and non-array paths', () => {
     const object = { '3': [{ 'b': { 'c': 3 } }] };
     expect(get(object, null, 'default')).toBe('default');
-    expect(get(object, null, 'default')).toBe('default');
     expect(get(object, undefined, 'default')).toBe('default');
-    //expect(get(object, '{}', 'default')).toBe('default');
     expect(get(object, 3, 'default')).toStrictEqual([{ 'b': { 'c': 3 } }]);
+    expect(get(object, true, 'default')).toStrictEqual([{ 'b': { 'c': 3 } }]); // Cannot handle boolean paths.
+    expect(get(object, '{}', 'default')).toBe('default'); // Cannot handle object paths.
   });
 
-  test('should handle complex paths where the key is', () => {
+  test('should correctly handle complex paths that include escaped characters', () => {
     const object = { 'a': [{ 'b': { 'c': 3, 'd\\e': 4, 'f': { 'g': 5 } } }] };
     expect(get(object, 'a[0].b.d\\e', 'default')).toBe(4);
   });
 
-  // Is that in the below that how function should behave?
   test('should return undefined when path does not exist and no default value is provided', () => {
     const object = { 'a': [{ 'b': { 'c': 3 } }] };
     expect(get(object, 'a.b.c')).toBeUndefined();
