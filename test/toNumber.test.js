190654151bbfac9f7d8af8cd8117d204e840a97c
masaoskari <masaoskari.linna@gmail.com>
2023-11-27 22:27:17 +0200

    toNumber function tests

--- test/toNumber.test.js
+++ test/toNumber.test.js
@@ -8,6 +8,10 @@ import 'jest-chain';
  * 1. Value is number
  * 2. Value is symbol
  * 3. Value is object
+ *  - With valueOf method that returns string that can be 
+ *    converted to number
+ *  - With valueOf method that returns another object
+ *  - Without value of method
  * 4. Value is string that can be converted to a number
  * 5. Value is string that cannot be converted to a number
  * 6. Value is string representing a binary number
@@ -15,7 +19,6 @@ import 'jest-chain';
  * 8. Value is string representing a hexadecimal number
  * 9. Value is null or undefined
  * 10. Value is boolean
- * 11. Value is 
  */
 
 describe('toNumber', () => {
@@ -31,27 +34,45 @@ describe('toNumber', () => {
     expect(toNumber(Symbol('symbol'))).toBeNaN();
   });
 
-  test('should convert object value to a number', () => {
-    //expect(toNumber({ 'a': 3.2 })).toBe(3.2);
+  test('should handle objects with valueOf method', () => {
     expect(toNumber({ valueOf: () => '3.2' })).toBe(3.2);
+  });
+
+  test('should handle objects without valueOf method', () => {
     expect(toNumber({})).toBeNaN();
+  });
+
+  test('should handle objects with valueOf method returning another object', () => {
+    const obj = {
+      valueOf: () => ({ foo: 'bar' }),
+    };
+    expect(toNumber(obj)).toBeNaN();
+  });
+
+  test('should handle objects where valueOf is not a function', () => {
+    const obj = {
+      valueOf: 'not a function',
+    };
+    expect(toNumber(obj)).toBeNaN();
+  });
+
+  test('should handle boolean values', () => {
+    expect(toNumber(true)).toBe(1);
+    expect(toNumber(false)).toBe(0);
+  });
+
+  test('should handle null and undefined', () => {
+    expect(toNumber(null)).toBe(0); // is null 0?
+    expect(toNumber(undefined)).toBeNaN();
   });
 
   test('should convert string value to a number', () => {
     expect(toNumber('3.2')).toBe(3.2);
-    expect(toNumber(' 3.2 ')).toBe(3.2);
     expect(toNumber('0b10')).toBe(2);
     expect(toNumber('0o10')).toBe(8);
     expect(toNumber('-0x1')).toBeNaN();
     expect(toNumber('')).toBe(0);
     expect(toNumber(' ')).toBe(0);
-  });
-
-  test('should convert non-string and non-object value to a number', () => {
-    expect(toNumber(null)).toBe(0);
-    expect(toNumber(undefined)).toBeNaN();
-    expect(toNumber(true)).toBe(1);
-    expect(toNumber(false)).toBe(0);
   });
 
   test('should handle large numbers', () => {
@@ -70,19 +91,13 @@ describe('toNumber', () => {
     expect(toNumber([])).toBe(0);
   });
 
-  test('should handle functions', () => {
-    expect(toNumber(() => {})).toBeNaN();
+  test('should handle BigInt values', () => {
+    const bigInt = BigInt(123);
+    expect(toNumber(bigInt)).toBe(123);
   });
 
   test('should handle objects with custom toString methods', () => {
     expect(toNumber({ toString: () => '3.2' })).toBe(3.2);
     expect(toNumber({ toString: () => 'abc' })).toBeNaN();
-  });
-
-  test('toNumber should return NaN when valueOf returns non-numeric value', () => {
-    const obj = {
-      valueOf: () => 'not a number'
-    };
-    expect(toNumber(obj)).toBeNaN();
   });
 });
