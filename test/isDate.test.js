e47991c415cf12fe20b20a63e98e4a1a0b3e57fb
masaoskari <masaoskari.linna@gmail.com>
2023-11-27 21:02:28 +0200

    isDate function tests implemented

--- test/isDate.test.js
+++ test/isDate.test.js
@@ -2,10 +2,13 @@ import isDate from '../src/isDate.js';
 import 'jest-extended';
 import 'jest-chain';
 
-
-describe('isDate', () => {
+describe('isDate without mock', () => {
   test('should return true for Date objects', () => {
     expect(isDate(new Date())).toBe(true);
+    expect(isDate(new Date('Mon April 23 2012'))).toBe(true);
+    expect(isDate(new Date(2022, 1, 1))).toBe(true);
+    expect(isDate(new Date(2022, 1, 1))).toBe(true);
+    expect(isDate(new Date('2022-01-01'))).toBe(true);
   });
 
   test('should return false for non-Date objects', () => {
@@ -16,8 +19,34 @@ describe('isDate', () => {
     expect(isDate(null)).toBe(false);
     expect(isDate(undefined)).toBe(false);
   });
+});
 
-  test('should return false for Date-like strings', () => {
-    expect(isDate('2022-01-01')).toBe(false);
+describe('isDate with nodeIsDate mocked', () => {
+  // Mocking nodeTypes.js isDate so that we can cover
+  // all the cases in isDate function.
+  beforeEach(() => {
+    jest.resetModules();
+    jest.doMock('../src/.internal/nodeTypes.js', () => ({
+      isDate: null,
+    }));
+  });
+
+  test('should return false if nodeIsDate is falsy and value is not a Date object', () => {
+    const isDate = require('../src/isDate.js').default;
+    expect(isDate('Mon April 23 2012')).toBe(false);
+    expect(isDate(123)).toBe(false);
+    expect(isDate({})).toBe(false);
+    expect(isDate([])).toBe(false);
+    expect(isDate(null)).toBe(false);
+    expect(isDate(undefined)).toBe(false);
+  });
+
+  test('should return true if nodeIsDate is falsy and value is a Date object', () => {
+    const isDate = require('../src/isDate.js').default;
+    expect(isDate(new Date())).toBe(true);
+    expect(isDate(new Date('Mon April 23 2012'))).toBe(true);
+    expect(isDate(new Date(2022, 1, 1))).toBe(true);
+    expect(isDate(new Date(2022, 1, 1))).toBe(true);
+    expect(isDate(new Date('2022-01-01'))).toBe(true);
   });
 });
